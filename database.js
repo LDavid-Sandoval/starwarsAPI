b2b77f2ed2e2b7688f128175542a14ceadb9bd3a
David Sandoval <42683508+dmagia7@users.noreply.github.com>
2019-10-04 23:49:57 -0500

    variables de entorno

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const URI = 'mongodb://admin:snqcdH5UjmpcsdBR@SG-starwars-26365.servers.mongodirector.com:27017/admin?ssl=true'
+const URI = process.env.DB_URL;
 mongoose.connect(URI,  { useNewUrlParser: true, useUnifiedTopology: true })
     .then(db => console.log('DB conectada'))
     .catch(err => console.log(err));
